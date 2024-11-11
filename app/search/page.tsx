ff8c096884c596fc30f142d030c835334619b34b
Mahmoud <mahmoud.abdulazizz.1911@gmail.com>
2024-11-11 03:56:47 +0200

    add Search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../componentes/header/Header";
 import { format } from "date-fns";
+import ListingCard from "../componentes/ListingCard";
+import { getSearchResult } from "../utils/api";
 type SearchParams = {
   location: string;
   startDate: string;
@@ -19,9 +21,56 @@ const SearchResult = async ({
     formatedEndDate = format(new Date(endDate), "dd MMMM yy");
   }
   const range = `${formatedStartDate} - ${formatedEndDate}`;
+  const filters = [
+    "Cancellation Flexibility",
+    "Type of Place",
+    "Price",
+    "Rooms and Beds",
+    "More filters",
+  ];
+  const searchResultData = await getSearchResult();
   return (
     <>
-      <Header placeholder={`${location} | ${range} | ${numOfGuests} guests`} />
+      <>
+        <Header
+          placeholder={`${location} | ${range} | ${numOfGuests} guests`}
+        />
+        <main>
+          <section>
+            <div className="container flex">
+              <div className="pt-14 pr-4">
+                <p className="text-xs">
+                  300+ Stays - {range} - for {numOfGuests} guests
+                </p>
+                <h1 className="text-3xl font-semibold mt-2 mb-6 ">
+                  Stays in {location}
+                </h1>
+                <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+                  {filters.map((filter) => (
+                    <button type="button" className="filter-btn" key={filter}>
+                      {filter}
+                    </button>
+                  ))}
+                </div>
+                <div>
+                  {searchResultData.map((listing: any) => (
+                    <ListingCard
+                      key={listing.title}
+                      img={listing.img}
+                      title={listing.title}
+                      location={listing.location}
+                      description={listing.description}
+                      price={listing.price}
+                      total={listing.total}
+                      star={listing.star}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
+      </>
     </>
   );
 };
--- app/utils/api.ts
+++ app/utils/api.ts
@@ -16,3 +16,12 @@ export const getLive = async () => {
     console.log(error);
   }
 };
+export const getSearchResult = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/b/5NPS`);
+    const searchResultDate = await res.json();
+    return searchResultDate;
+  } catch (error) {
+    console.log(error);
+  }
+};
